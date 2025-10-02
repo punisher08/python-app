a9d2b389e0f642443672ca206f03aceb71e9c9e3
jhomignacio08@gmail.com <webdev@onlinemarketinggurus.com.au>
2025-10-02 19:39:44 +1000

    updated files

--- Django/ecomproject/userauths/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from userauths.models import User
-
-# Register your models here.
-class UserAdmin(admin.ModelAdmin):
-    list_display = ['email','username','first_name','last_name']
-
-admin.site.register(User,UserAdmin)
